70bf160760677f1c60d76df8890d650679020ebc
GhosTHunTer2965 <nayan23ajay@gmail.com>
2025-10-06 16:19:10 +0530

    Add files via upload

---
+++ .next/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./build/chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("build/chunks/[turbopack-node]_transforms_postcss_ts_55b4a926._.js")
+R.c("build/chunks/[root-of-the-server]__89ad1716._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Downloads/zephvion-master (7)/zephvion-master/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Downloads/zephvion-master (7)/zephvion-master/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
