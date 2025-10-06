f24e54a96bf2d2193a94d782803a4ed2dfee26b9
GhosTHunTer2965 <nayan23ajay@gmail.com>
2025-10-06 16:28:00 +0530

    Add files via upload

---
+++ .next/build/chunks/[turbopack-node]_transforms_postcss_ts_55b4a926._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Downloads/zephvion-master (7)/zephvion-master/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "build/chunks/d5a3e_5cf2ac5b._.js",
+  "build/chunks/[root-of-the-server]__3abf26a4._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Downloads/zephvion-master (7)/zephvion-master/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
